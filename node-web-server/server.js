f0ef4859b6bc62cb73f11b49bab4b98a1fa25734
Nik Martinez <nikolaus.c.martinez@gmail.com>
2017-10-11 17:47:57 -0500

    added the next() to middleware

--- node-web-server/server.js
+++ node-web-server/server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const hbs = require('hbs');
+const fs = require('fs');
 
 // Make the express app
 var app = express();
@@ -10,18 +11,44 @@ app.set('view engine', 'hbs');
 // hbs tells handlebars to use partials
 hbs.registerPartials(__dirname + '/views/partials'); // __dirname stores the path to your projects directory
 
+// next tells you when your middleware is done
+app.use((req, res, next) => {
+  var now = new Date().toString(); // Get time
+  // Store the request type and url
+  var log = now + ': ' + req.method + ' ' + req.url
+  console.log(log);
+
+  // Store the time request type and url in a file
+  fs.appendFile('server.log', log + '\n', (err) => {
+    if (err) {
+      console.log('Unable to append to server.log');
+    }
+  })
+  // This allows you to move on the next piece of middleware
+  next();
+});
+
+// This will stop all middleware since it doesn't have the next() function
+/*app.use((req, res, next) => {
+  res.render('maintenance.hbs');
+});
+*/
+
+// Add middleware public
+app.use(express.static(__dirname + '/public')); // __dirname stores the path to your projects directory
+
 // hbs tells handlebars to use this helpers for getCurrentYear.
 // This helps you reduce repeated code. You use these in your partials.
 hbs.registerHelper('getCurrentYear', () => {
   return new Date().getFullYear()
 })
 
+// hbs tells handlebars to use this helpers for screamIt.
+// This helps you reduce repeated code. You use the returned javaScrip function in your partials.
 hbs.registerHelper('screamIt', (text) => {
   return text.toUpperCase();
 })
 
-// Add middleware
-app.use(express.static(__dirname + '/public')); // __dirname stores the path to your projects directory
 
 // Creating a home route
 app.get('/', (req, res) => {  // request , response
