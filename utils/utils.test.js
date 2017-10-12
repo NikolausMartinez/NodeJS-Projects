1aee41981795d2c0e54756a666423b6b17456b46
Nik Martinez <nikolaus.c.martinez@gmail.com>
2017-10-11 19:41:19 -0500

    comments

--- utils/utils.test.js
+++ utils/utils.test.js
@@ -2,16 +2,22 @@
 
 const utils = require('./utils');
 
-// Behavior driven development
+// it() is Behavior driven testing with mocha
 it('Should add two numbers', () => {
+  // Call the function were testing.
   var res = utils.add(33, 11);
+
+  // Throw error if result is wrong.
   if (res !== 44) {
     throw new Error('Expected 44 but got ' + res);
   };
 });
 
 it('Should square the number', () => {
+  // Call the function were testing.
   var res = utils.square(5, 5);
+
+  // Throw error if result is wrong.
   if (res !== 25){
     throw new Error('Expected 25 but got ' + res);
   };
